a96fe8a9da56185cb8616232b0253e66182cc30c
Jonathan Velasquez <johedvelca@gmail.com>
2025-07-03 22:31:49 -0400

    Submit data to chain logic added. AddNewList().

--- my-first-dapp/frontend/App.tsx
+++ my-first-dapp/frontend/App.tsx
@@ -1,25 +1,31 @@
 import { Header } from "@/components/Header";
 import { TopBanner } from "@/components/TopBanner";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useWallet, InputTransactionData } from "@aptos-labs/wallet-adapter-react";
 import { useEffect, useState } from "react";
 import { MODULE_ADDRESS } from "./constants";
 import { aptosClient } from "./utils/aptosClient";
 import { Button } from "./components/ui/button";
  
 function App() {
-  const { account } = useWallet();
+  const { account, signAndSubmitTransaction } = useWallet();
 
   useEffect(() => {
     fetchList();
   }, [account?.address]);
 
-  const [accountHasList, setAccountHasList] = useState<boolean>(false);
+  const [accountHasList, setAccountHasList] = useState<boolean>(false); // State to track if the account has a todo list
+  const [transactionInProgress, setTransactionInProgress] = useState<boolean>(false);
+  const moduleAddress = MODULE_ADDRESS; //smart contract address
+  
 
+
+  // Fetch the todo list for the connected account
   const fetchList = async () => {
-    if (!account) return [];
-    const moduleAddress = MODULE_ADDRESS;
+    if (!account) return []; // If no account is connected, return an empty array
 
     try {
+      // Attempt to get the TodoList resource for the connected account
+      // If the resource exists, it means the account has a todo list
       const todoListResource = await aptosClient().getAccountResource(
         {
           accountAddress:account?.address,
@@ -27,8 +33,32 @@ function App() {
         }
       );
       setAccountHasList(true);
-    } catch (e: any) {
+    } catch (e: any) { // If it does not exist, an error will be thrown
       setAccountHasList(false);
+    }
+  };
+
+
+  // Function to add a new todo list for the connected account
+  const addNewList = async () => {
+    if (!account) return [];
+  
+    const transaction:InputTransactionData = {
+        data: {
+          function:`${moduleAddress}::todolist::create_list`,
+          functionArguments:[]
+        }
+      }
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(transaction);
+      // wait for transaction
+      await aptosClient().waitForTransaction({transactionHash:response.hash});
+      setAccountHasList(true);
+    } catch (error: any) {
+      setAccountHasList(false);
+    } finally {
+      setTransactionInProgress(false);
     }
   };
 
@@ -39,7 +69,12 @@ function App() {
       <div className="flex items-center justify-center flex-col">
         {!accountHasList && (
           <div className="flex items-center justify-center flex-col">
-            <Button>Add new list</Button>
+            <Button
+              onClick={addNewList}
+              disabled={transactionInProgress}
+            >
+              Add new list
+            </Button>
           </div>
         )}
       </div>
