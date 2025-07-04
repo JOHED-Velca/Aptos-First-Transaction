522ca92bf2cc38ca35e2577622d4c87daa1c3e97
Jonathan Velasquez <johedvelca@gmail.com>
2025-07-03 21:55:50 -0400

    fetchList() added

--- my-first-dapp/frontend/App.tsx
+++ my-first-dapp/frontend/App.tsx
@@ -1,7 +1,36 @@
 import { Header } from "@/components/Header";
 import { TopBanner } from "@/components/TopBanner";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useEffect, useState } from "react";
+import { MODULE_ADDRESS } from "./constants";
+import { aptosClient } from "./utils/aptosClient";
  
 function App() {
+  const { account } = useWallet();
+
+  useEffect(() => {
+    fetchList();
+  }, [account?.address]);
+
+  const [accountHasList, setAccountHasList] = useState<boolean>(false);
+
+  const fetchList = async () => {
+    if (!account) return [];
+    const moduleAddress = MODULE_ADDRESS;
+    
+    try {
+      const todoListResource = await aptosClient().getAccountResource(
+        {
+          accountAddress:account?.address,
+          resourceType:`${moduleAddress}::todolist::TodoList`
+        }
+      );
+      setAccountHasList(true);
+    } catch (e: any) {
+      setAccountHasList(false);
+    }
+  };
+
   return (
     <>
       <TopBanner />
